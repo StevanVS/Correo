44f3e6566da0a9135ebb308f02512253fd39015a
StevanVS <stevanvs@outlook.com>
2022-08-16 13:43:05 -0500

    Base de datos actualizada

--- database/correo.sql
+++ database/correo.sql
@@ -31,6 +31,13 @@ create table drafts (
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+create table history (
+	id int auto_increment primary key,
+    user_id int,
+    foreign key (user_id) references users(id)
+);
+
+
 create table labels (
 	id varchar(50) primary key,
     name varchar(50) not null
@@ -45,30 +52,45 @@ create table user_emails (
 	user_id int,
     label_id varchar(50),
     email_id int,
-    primary key (user_id, label_id, email_id),
+    draft_id int,
+    primary key (user_id, label_id, email_id, draft_id),
     foreign key (user_id) references users(id),
-    foreign key (label_id) references labels(id),
-    foreign key (email_id) references emails(id)
+    foreign key (label_id) references labels(id)
 );
 
 delimiter //
-create trigger assign_email_label
-after insert on emails
-for each row
+create trigger insert_email_trigger 
+after insert on emails for each row
 begin
-	insert into user_emails values (new.to_user, 'INBOX', new.id);
-    insert into user_emails values (new.from_user, 'SENT', new.id);
+	insert into user_emails values (new.to_user, 'INBOX', new.id, -1);
+    insert into user_emails values (new.from_user, 'SENT', new.id, -1);
+    
+    insert into history (user_id) value (new.to_user);
+    insert into history (user_id) value (new.from_user);
 end//
 
-create trigger assign_draft_label
-after insert on drafts
-for each row
+create trigger insert_draft_trigger
+after insert on drafts for each row
 begin
-    insert into user_emails values (new.from_user, 'DRAFT', new.id);
+    insert into user_emails values (new.from_user, 'DRAFT', -1, new.id);
+    insert into history (user_id) value (new.from_user);
 end//
 
+/*
+create procedure delete_received_email (in user_id int, in email_id int)
+begin
+	delete from user_emails t where t.user_id = user_id and t.label_id <> 'SENT' and t.email_id = email_id;
+end//
 
-create procedure get_emails (in userId int) 
+create procedure delete_sent_email (in user_id int, in email_id int)
+begin
+	delete from user_emails t where t.user_id = user_id and t.label_id = 'SENT' and t.email_id = email_id;
+end//
+
+*/
+
+
+-- create procedure get_emails (in userId int) 
 
 
 delimiter ;
@@ -86,4 +108,17 @@ insert into emails (id, from_user, to_user, subject, message) values
 	(null, 3, 4, 'test3', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium in enim voluptate eaque aperiam labore quaerat eveniet eius, sunt mollitia quos iure consequuntur magnam similique nesciunt, cumque itaque veniam ad.');
 
 insert into emails (from_user, to_user, subject, message, unread) values
-	(3, 2, 'Mi Asunto', 'Mi mensaje', 0);
+	(2, 2, 'Mi Asunto', 'Mi mensaje 2 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium in enim volupta', 0);
+
+insert into drafts (from_user, to_user, subject, message) values
+ 	(2, 'svelez1@email.com', 'Hola', 'Este es mi mensaje'),
+     (2, 'aa@ff', null, null);	
+
+
+-- select * from user_emails;
+
+
+-- select * from user_emails where user_id = 2;
+
+
+
